2232b9fb61665ec445738c11d9ac468f7fa1a04f
陈伟成 <804699297@qq.com>
2019-08-23 17:13:13 +0800

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,103 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <el-button @click="dialogVisible = true">新增</el-button>
+    <el-table
+      border
+      highlight-current-row
+      :data="data">
+      <el-table-column
+        label="空间名称"
+        prop="bucket">
+      </el-table-column>
+      <el-table-column
+        label="域名"
+        prop="bucket">
+      </el-table-column>
+      <el-table-column label="操作">
+        <template>
+          <el-button type="primary">管理</el-button>
+          <el-button type="danger">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-dialog
+      title="新增"
+      :visible.sync="dialogVisible"
+      width="30%">
+      <div>
+        <el-form ref="form" :model="form" label-width="80px">
+          <el-form-item prop="name" label="名称">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="地域">
+            <el-select v-model="form.zone" placeholder="请选择">
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
+          </el-form-item>
+        </el-form>
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addBucket">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data () {
+    return {
+      data: [],
+      options: [],
+      dialogVisible: false,
+      form: {
+        zone: '',
+        name: ''
+      }
+    }
+  },
+  methods: {
+    addBucket () {
+      axios.post(`http://ad.huishimed.com:3000/buckets`, this.form)
+        .then(res => {
+          // eslint-disable-next-line no-console
+          console.log(res)
+        })
+    }
+  },
+  created () {
+    axios.get(`http://ad.huishimed.com:3000/buckets`)
+      .then(res => {
+        // eslint-disable-next-line no-console
+        console.log(res)
+        if (res.status === 200) {
+          this.data = res.data
+        }
+      })
+
+    axios.get(`http://ad.huishimed.com:3000/options/zone`)
+      .then(res => {
+        // eslint-disable-next-line no-console
+        console.log(res)
+        if (res.status === 200) {
+          this.options = res.data
+        }
+      })
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
