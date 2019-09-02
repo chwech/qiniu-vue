780150f1fdac30538b64bf736f84a77011d71ca6
陈伟成 <804699297@qq.com>
2019-09-02 14:16:44 +0800

    获取存储空间域名

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,9 +2,16 @@
   <div class="hello">
     <el-button @click="dialogVisible = true">新增</el-button>
     <el-table
+      @expand-change="onExpandChange"
       border
       highlight-current-row
       :data="data">
+      <el-table-column
+        type="expand">
+        <template v-slot:default="{ row }">
+          <div>域名：<span v-for="(d, i) of row.domain" :key="i">{{ i + ':' + d }}</span></div>
+        </template>
+      </el-table-column>
       <el-table-column
         label="空间名称"
         prop="bucket">
@@ -64,10 +71,34 @@ export default {
       form: {
         zone: '',
         name: ''
-      }
+      },
+      domain: []
     }
   },
   methods: {
+    onExpandChange (row) {
+      axios.get(`http://ad.huishimed.com:3000/domain/${row.bucket}`)
+        .then(res => {
+          const { data: { code, msg: message, data } } = res
+          if (code === 20000) {
+            this.$message({
+              type: 'message',
+              message
+            })
+            // TODO: 优化显示
+            row.domain = data
+          } else {
+            this.$message({
+              type: 'error',
+              message: '获取失败'
+            });  
+          }
+        })
+        .catch(err => {
+          // eslint-disable-next-line no-console
+          console.log('err', err)
+        })
+    },
     getList() {
       axios.get(`http://ad.huishimed.com:3000/bucket`)
         .then(res => {
